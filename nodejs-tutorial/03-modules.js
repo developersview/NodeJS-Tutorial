104f3e917ff945d8747345e2010060edf869be33
developersview <pcslg1998@gmail.com>
2023-07-07 02:19:09 +0530

    alternative way of module exports

--- nodejs-tutorial/03-modules.js
+++ nodejs-tutorial/03-modules.js
@@ -1,5 +1,9 @@
 const greet = require('./05-utils');
 const names = require('./04-names');
+const data = require('./06-alternative-flavors');
+const mg = require('./07-mindgranade');
+
 
 greet(names.adam);
 greet(names.pranoy);
+console.log(data);
